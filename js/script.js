74d7be33ca392021fcae4a809a5ec042c51e8b6f
hannahchrisgower <hannahchrisgower@gmail.com>
2026-03-29 21:14:06 -0400

    step 7 again

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 console.log("script.js loaded");
-let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+let endpoint = "https://api.giphy.com/v1/gifs/search?api_key=C7u6nYC8PXnTdocGEz8KcCwJgKUKZkzb&q=cats&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
 
 let data = fetch(endpoint);
 let images = [data]
